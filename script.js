8e8be452f66992574b3c783dd2675e0c60e1281b
quler1303 <j.fabjanska@gmail.com>
2023-08-19 08:40:51 +0200

    d

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var svgElement = document.getElementById("click");
-    var audio = document.getElementById("happy");
-
-    svgElement.addEventListener("click", function() {
-        audio.play();
-    });
-    svgElement.addEventListener("touchstart", playAudio);
-});
